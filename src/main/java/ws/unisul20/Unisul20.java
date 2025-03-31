273132003a6c1cfa1bae28a5a7f5d740c8f50e7b
feeh <feeh@DESKTOP-2FBFR4I>
2025-03-31 14:37:35 -0300

    exercicio for while 20 java

---
+++ src/main/java/ws/unisul20/Unisul20.java
@@ -0,0 +1,11 @@
+
+package ws.unisul20;
+import javax.swing.JOptionPane;
+public class Unisul20 {
+
+    public static void main(String[] args) {
+       for (int i = 1; i <= 10; i++) {
+           JOptionPane.showMessageDialog(null, i);
+       }
+    }
+}
